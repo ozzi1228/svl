6c9ed988dd8611aee6ea6d0be661c173b4b37ead
이언지 <lstory1997l@gmail.com>
2026-05-20 14:36:34 +0900

    SSAFY 공통 프로젝트 Readme 작성

---
+++ src/test/java/com/eonji/ssafy/a303/svl_refactored/SvlRefactoredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eonji.ssafy.a303.svl_refactored;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvlRefactoredApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
